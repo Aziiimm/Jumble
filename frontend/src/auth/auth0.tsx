ea2d75830b8054c0e1da19c35cc4136f1ffe5bc4
Azim Rahat <128262789+Aziiimm@users.noreply.github.com>
2025-09-18 02:36:09 -0400

    Update auth0.tsx
    Signed-off-by: Azim Rahat <128262789+Aziiimm@users.noreply.github.com>

--- frontend/src/auth/auth0.tsx
+++ frontend/src/auth/auth0.tsx
@@ -7,8 +7,8 @@ export default function Auth0ProviderWithNavigate({
 }) {
   return (
     <Auth0Provider
-      domain={import.meta.env.VITE_AUTH0_DOMAIN!}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID!}
+      domain={import.meta.env.AUTH0_DOMAIN!}
+      clientId={import.meta.env.AUTH0_CLIENT_ID!}
       authorizationParams={{
         redirect_uri: window.location.origin,
         audience: import.meta.env.VITE_AUTH0_AUDIENCE,
